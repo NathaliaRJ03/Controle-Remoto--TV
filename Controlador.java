565ad3ffd94ed410974e3f1456296ab293e727c3
Nathália Duarte <144814239+NathaliaRJ03@users.noreply.github.com>
2023-09-12 12:55:56 -0300

    Add files via upload

---
+++ Controlador.java
@@ -0,0 +1,16 @@
+package aula1;
+public interface Controlador {
+ public abstract void ligar();
+ public abstract void desligar();
+ public abstract void abrirMenu();
+ public abstract void fecharMenu();
+ public abstract void maisVolume();
+ public abstract void menosVolume ();
+ public abstract void LigarMudo ();
+ public abstract void desligarMudo ();
+ public abstract void play ();
+ public abstract void pause ();
+ public abstract void MudardeCanal ();
+ public abstract void MudarparaAv();
+ public abstract void MudarparaHdmi (); 
+}
